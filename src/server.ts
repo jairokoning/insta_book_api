4e2eee14d56a1201bdebaf9675e058109e0952a5
Jairo Koning <jairo.koning@gmail.com>
2022-01-17 16:27:35 -0300

    conf: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3333, () => console.log("Server is running"));
